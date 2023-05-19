adde12fa002fc11a7eeae40a75aa92522cd6efa3
AbdullahWB <abdullah917828@gmail.com>
2023-05-19 22:12:07 +0600

    make search in my toy page

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -6,6 +6,7 @@ const MyToys = () => {
     const { user } = useContext(AuthContext)
     // console.log(user.email);
     const [myToys, setMyToys] = useState([])
+    const [search, setSearch] = useState("")
     useEffect(() => {
         fetch(`http://localhost:3000/myToy/${user?.email}`)
             .then(res => res.json())
@@ -13,9 +14,31 @@ const MyToys = () => {
                 console.log(data);
                 setMyToys(data);
             })
-    },[user])
+    }, [user])
+
+    const handleSearch = () => {
+        fetch(`http://localhost:3000/itemSearch/${search}`)
+            .then(res => res.json())
+            .then(data => {
+                setMyToys(data);
+            })
+    }
+
+
     return (
         <div className='max-w-7xl mx-auto'>
+            <div className="form-control my-[50px]">
+                <div className="input-group">
+                    <input
+                        type="text"
+                        onChange={(e)=> setSearch(e.target.value)}
+                        placeholder="Search…"
+                        className="input input-bordered" />
+                    <button onClick={handleSearch} className="btn btn-square">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+                </div>
+            </div>
             <div className="overflow-x-auto w-full">
                 <table className="table w-full">
                     {/* head */}
@@ -37,7 +60,7 @@ const MyToys = () => {
                                 myToy={myToy}
                                 index={index}
                             ></ToyCard>)
-                       }
+                        }
                     </tbody>
                 </table>
             </div>
